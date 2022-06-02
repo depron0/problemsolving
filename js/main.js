cbc9fe59b601b287682762a4153b1e80af2541e2
depron0 <sceneronja@gmail.com>
2022-06-02 09:17:12 +0200

    Added 4 new challenges

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function evenOrOdd(num) {
+  if(num % 2 === 0){return 'Even'}
+  else{return 'Odd'}
+}
+      
+
+// Create a function that can transform a number into a string.
+function numToStr(num) {
+    return num.toString()
+}
+
+
+// Create a function that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToStr(bool){
+  return bool ? 'Yes' : 'No'
+}
+
+
+// Create a function that can transform a string into a number.
+function strToNum(str){
+    return Number(str)
+}
+
+
