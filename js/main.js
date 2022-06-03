ecbc9c4f4bfdbf68dd0fe2182888f1c4902134d0
depron0 <sceneronja@gmail.com>
2022-06-03 09:57:07 +0200

    added 5 more challenges, updated some captions

--- js/main.js
+++ js/main.js
@@ -1,25 +1,56 @@
-// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// take an integer as an argument and return "Even" for even numbers or "Odd" for odd numbers
 function evenOrOdd(num) {
   if(num % 2 === 0){return 'Even'}
   else{return 'Odd'}
 }
       
 
-// Create a function that can transform a number into a string.
+// transform a number into a string
 function numToStr(num) {
     return num.toString()
 }
 
 
-// Create a function that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+// take a boolean value and return a "Yes" string for true, or a "No" string for false
 function boolToStr(bool){
   return bool ? 'Yes' : 'No'
 }
 
 
-// Create a function that can transform a string into a number.
+// Create a function that can transform a string into a number
 function strToNum(str){
     return Number(str)
 }
 
+
+// given an integer or a floating-point number, find its opposite
+
+function oppositeNum(num) {
+  return (num = -num)
+  // BETTER: just return(-num)
+}
+
+
+// given a number and have to make it negative
+function makeNegative(num) {
+  return -Math.abs(num)
+}
+
+
+// reverse the string passed into it
+function reverseStr(str) {
+  return str.split("").reverse().join("")
+}
+
+
+// repeat the given string string exactly n times
+function repeatStr(n, str) {
+  return str.repeat(n)
+}
+
+// remove the first and last characters of a string
+function firstAndLast(str) {
+  return str.slice(1, str.length -1)
+  // BETTER: str.slice(1, -1)
+}
 
