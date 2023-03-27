86600a7bd4ce1a55db9377d7f67ad026f438deb2
depron0 <sceneronja@gmail.com>
2023-03-27 11:19:06 +0200

    added new file for difficulty level 6, and added  two challenges

---
+++ codewars/6 kyu.js
@@ -0,0 +1,10 @@
+// sum all multiples of 3 or 5 below given number (not inclusive)
+// if given negative number, return 0
+function sumMultiples(num){
+    if(num > 0) {
+      numArr = Array.from({length: num - 1}, (_, index) => index + 1)
+      return numArr.filter(x => x % 3 == 0 || x % 5 == 0).reduce((acc,cur) => acc + cur, 0)
+    } else {
+      return 0
+    }
+  }
